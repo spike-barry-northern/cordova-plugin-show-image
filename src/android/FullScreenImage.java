314ba66c1a265d4b9a525c3eb7c1a99890f2a67e
Barry Northern <barry.n@spikeglobal.com>
2019-11-19 17:08:36 +0000

    fix

--- src/android/FullScreenImage.java
+++ src/android/FullScreenImage.java
@@ -156,7 +156,7 @@ public class FullScreenImage extends CordovaPlugin {
 	private void showImage (File file)  {
 
 		Log.v(FullScreenImage.LOG_TAG, "show saved image");
-		Uri path = Uri.fromFile(file);
+		Uri path = "file://" + Uri.fromFile(file);
 		Log.v(FullScreenImage.LOG_TAG, path.getPath());
 		Intent intent = new Intent(Intent.ACTION_VIEW);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
