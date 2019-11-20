cca0ccbaf392af8be9740b3f2ae996ef3a1ec162
Barry Northern <barry.n@spikeglobal.com>
2019-11-20 17:32:36 +0000

    photoviewer

---
+++ www/PhotoViewer.d.ts
@@ -0,0 +1,15 @@
+declare var exec: any;
+interface IPhotoViewerOptions {
+    share?: boolean;
+    closeButton?: boolean;
+    copyToReference?: boolean;
+    headers?: string;
+    piccasoOptions?: {
+        fit?: boolean;
+        centerInside?: boolean;
+        centerCrop?: boolean;
+    };
+}
+declare class PhotoViewer {
+    static show(url: string, title?: string, options?: IPhotoViewerOptions): void;
+}
